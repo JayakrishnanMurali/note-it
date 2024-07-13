e6172edc24cb84476a5a85c823c3bdd8b607af53
Jayakrishnan M <jayakrishnan1012@gmail.com>
2024-07-14 03:07:23 +0530

    chore: Add Convex client provider to app layout

--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -1,9 +1,15 @@
 "use client";
 
+import { Spinner } from "@/components/spinner";
 import { Button } from "@/components/ui/button";
+import { SignInButton } from "@clerk/clerk-react";
+import { useConvexAuth } from "convex/react";
 import { ArrowRightIcon } from "lucide-react";
+import Link from "next/link";
 
 export const Heading = () => {
+  const { isAuthenticated, isLoading } = useConvexAuth();
+
   return (
     <div className="max-w-3xl space-y-4">
       <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
@@ -16,10 +22,29 @@ export const Heading = () => {
         and beautiful. Get started today!
       </h3>
 
-      <Button>
-        Enter Knotit
-        <ArrowRightIcon className="h-4 w-4 ml-2" />
-      </Button>
+      {isLoading && (
+        <div className="flex justify-center items-center w-full">
+          <Spinner size="lg" />
+        </div>
+      )}
+
+      {!isAuthenticated && !isLoading && (
+        <SignInButton mode="modal">
+          <Button size="sm">
+            Get Knotit free
+            <ArrowRightIcon className="h-4 w-4 ml-2" />
+          </Button>
+        </SignInButton>
+      )}
+
+      {isAuthenticated && !isLoading && (
+        <Button asChild>
+          <Link href="/documents">
+            Enter Knotit
+            <ArrowRightIcon className="h-4 w-4 ml-2" />
+          </Link>
+        </Button>
+      )}
     </div>
   );
 };
